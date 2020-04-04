02532096e0a0fd61c128f53464b3ecc306a96e99
Hazmi <whitespaces@gmail.com>
2020-04-04 10:22:35 +0700

    redirect index to indonesia

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,7 @@
-<template>
-  <div :class="$style.container">
-    <div :class="$style.confirmed">
-      <Graph
-        title="Confirmed"
-        total="1528"
-        font-size="80"
-        color="242, 201, 76"
-      />
-    </div>
-    <div :class="$style.recovered">
-      <Graph
-        title="Recovered"
-        total="81"
-        font-size="60"
-        color="111, 207, 151"
-      />
-    </div>
-    <div :class="$style.death">
-      <Graph title="Death" total="136" font-size="60" color="235, 87, 87" />
-    </div>
-    <div :class="$style.date">
-      <Date ts="1585612800000" />
-    </div>
-  </div>
-</template>
-
 <script>
-import Graph from '~/components/Graph.vue';
-import Date from '~/components/Date.vue';
-
 export default {
-  components: {
-    Graph,
-    Date
+  fetch({ params, redirect }) {
+    redirect(301, '/indonesia');
   }
 };
 </script>
-<style module>
-.container {
-  flex: 1;
-  display: grid;
-  padding: 10px;
-  grid-gap: 10px;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
-}
-.confirmed {
-  display: flex;
-  grid-column: 1/3;
-}
-.recovered {
-  display: flex;
-}
-.death {
-  display: flex;
-}
-.date {
-  display: flex;
-  grid-column: 1/3;
-}
-</style>
