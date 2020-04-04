4a8b655d6719aade1a8ec5b078b41174d954e2cd
Hazmi <whitespaces@gmail.com>
2020-04-04 12:45:18 +0700

    add page title on index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,18 @@
 export default {
   fetch({ params, redirect }) {
     redirect(301, '/indonesia');
+  },
+  head() {
+    return {
+      title: 'Covid19 in Indonesia',
+      meta: [
+        {
+          hid: 'Covid19 in Indonesia',
+          name: 'Covid19 in Indonesia',
+          content: 'Covid19 in Indonesia'
+        }
+      ]
+    };
   }
 };
 </script>
