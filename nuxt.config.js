66a84fda96a5eefcf53fa7fda593109f3c39366d
Hazmi <whitespaces@gmail.com>
2020-04-07 10:02:30 +0700

    update favicon and prevent zoom in mobile

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,24 @@ export default {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, user-scalable=no'
+      },
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '100x100',
+        href: '/icon-100x100.png'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
