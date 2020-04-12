b74fadebb82dde531118c9865191f7092d8ad758
Hazmi <whitespaces@gmail.com>
2020-04-12 18:46:32 +0700

    add lang attr and update project desc

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ export default {
    ** Headers of the page
    */
   head: {
+    htmlAttrs: { lang: 'id' },
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
