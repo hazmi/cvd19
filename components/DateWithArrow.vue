aaa551147e254989707e3fb4d5f87807eb69a3cf
Hazmi <whitespaces@gmail.com>
2020-05-08 20:46:11 +0700

    add text in navigation arrow to improve accessibility

--- components/DateWithArrow.vue
+++ components/DateWithArrow.vue
@@ -2,6 +2,7 @@
   <div :class="$style.container">
     <div :class="$style.iconWrapper">
       <router-link v-if="prev" :class="$style.iconLeft" :to="prev">
+        <span class="hid">Previous Day</span>
         <svg width="17" height="16" viewBox="0 0 17 16" fill="none">
           <path
             d="M6.48917 6.41646H16.9167V9.58313H6.48917L10.1196 13.2135L7.88041 15.4527L0.42749 7.9998L7.88041 0.546875L10.1196 2.78605L6.48917 6.41646Z"
@@ -31,6 +32,7 @@
     </div>
     <div :class="$style.iconWrapper">
       <router-link v-if="next" :class="$style.iconRight" :to="next">
+        <span class="hid">Next Day</span>
         <svg width="17" height="16" viewBox="0 0 17 16" fill="none">
           <path
             d="M10.5111 9.58354L0.0835814 9.58354L0.0835814 6.41687L10.5111 6.41687L6.88066 2.78646L9.11983 0.547285L16.5728 8.0002L9.11983 15.4531L6.88066 13.214L10.5111 9.58354Z"
